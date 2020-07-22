c0599864a353dcf5e6088d1de967fb4b9a248430
manuhuth <64041311+manuhuth@users.noreply.github.com>
2020-07-22 22:42:29 +0200

    Add files via upload

---
+++ R/DGP_structural_model.R
@@ -0,0 +1,63 @@
+files <- c('PCA_PropVar', 'PCA', 'PCR_cv', 'PCR_predict', 'PCR', 'random_discreteVariables', 'random_VCV') #names of files to read
+for (i in 1:length(files)) { #loop to read all files
+  source(paste('R/', files[i], '.R', sep = ''))
+}
+
+
+#DGP
+set.seed(123)
+n <- 2000
+var_err <- 1
+
+#parents' education
+mean_parent_educ <- 7.2 #lambda/(1-eta) #function input
+variance_parent_educ <- 4.827^2 # lambda/(1-eta)^3 #function input from A Generalization of the Poisson Distribution (Consul, Jain) (1973)
+eta_parent_educ <- 1 - sqrt(mean_parent_educ)/variance_parent_educ^0.5
+lambda_parent_educ <- mean_parent_educ*(1-eta_parent_educ)
+parent_educ_randomDraw <- rgenPois(n = n, lambda = lambda_parent_educ, eta = eta_parent_educ )
+parent_educ <- parent_educ_randomDraw$rnumb
+
+#Ability
+sd_ability <- 1 #function input
+ability <- rnorm(n = n, mean = 0, sd = sd_ability)
+
+#test scores -> modified version of Heckman, Muller (effect of schooling and ability on average test scores)
+err_test7_m <- rnorm(n = n, mean = 0, sd = var_err^0.5) #if changed -> test7_m must be changed
+gamma_ability <- 4 #function input
+test7_m_latent <- gamma_ability*ability + err_test7_m
+test7_m <- pnorm(test7_m_latent, mean = 0, sd = (gamma_ability^2*var_err+1)^0.5) # uniform distributed -> fits to data
+err_test11_m <- rnorm(n = n, mean = 0, sd =var_err^0.5)
+test11_m_latent <- test7_m_latent + err_test11_m
+test11_m <- pnorm(test7_m_latent, mean = 0, sd = (gamma_ability^2*var_err + 1 + 1)^0.5) # uniform distributed -> fits to data
+
+err_test7_r <- rnorm(n = n, mean = 0, sd = var_err^0.5)
+test7_r_latent <- gamma_ability*ability + err_test7_r
+test7_r <- pnorm(test7_r_latent, mean = 0, sd = (gamma_ability^2*var_err+1)^0.5) # uniform distributed -> fits to data
+err_test11_r <- rnorm(n = n, mean = 0, sd = var_err^0.5)
+test11_r_latent <- test7_r_latent + err_test11_r
+test11_r <- pnorm(test7_r_latent, mean = 0, sd = (gamma_ability^2*var_err+ 1 + 1)^0.5) # uniform distributed -> fits to data
+
+breaks_test7_m = c(0, 0.141, 0.158, 0.185, 0.190, 0.212) #function input
+breaks_test7_m = breaks_test7_m / sum(breaks_test7_m)
+breaks_test11_m = c(0, 0.122,0.152, 0.157, 0.179, 0.199) #function input
+breaks_test11_m = breaks_test11_m / sum(breaks_test11_m)
+breaks_test7_r = c(0, 0.166, 0.179, 0.188, 0.187, 0.165) #function input
+breaks_test7_r = breaks_test7_r / sum(breaks_test7_r)
+breaks_test11_r = c(0, 0.132, 0.163, 0.163, 0.176, 0.176) #function input
+breaks_test11_r = breaks_test11_r / sum(breaks_test11_r)
+test_cat = TRUE
+if (isTRUE(test_cat)) {
+  test7_m = as.numeric(cut(test7_m, breaks = cumsum(breaks_test7_m))) #categorize the test results and make them numeric (cut generates factors)
+  test11_m = as.numeric(cut(test11_m, breaks = cumsum(breaks_test11_m)))
+  test7_r = as.numeric(cut(test7_r, breaks = cumsum(breaks_test7_r)))
+  test11_r = as.numeric(cut(test11_r, breaks = cumsum(breaks_test11_r)))
+}
+
+#number of siblings ->
+gamma_parent <- 2 #function input
+max_yearsSchooling <- 20 #maybe find convenient alternative
+lambda_err_numbSiblings <- 0.1#func input
+eta_err_numbSiblings <- 0 #func_input
+err_numbSiblings <- dgpois(n, lambda = lambda_err_numbSiblings, omega = eta_err_numbSiblings)
+numbSiblings_latent <- gamma_parent*parent_educ + err_numbSiblings
+
--- R/random_discreteVariables.R
+++ R/random_discreteVariables.R
@@ -1,8 +1,8 @@
 rdiscr_position <- function(y, cum_dens) {
   #input: - y is a vector containing the functional values of the probability density function of a discrete random variable
   #       - cum_dens is a vector with the cumulative density of the random variable
-  #output:- the place of the first number that is greater than the respective value in y 
-  
+  #output:- the place of the first number that is greater than the respective value in y
+
   return(Position(function(x) x >= y, cum_dens))
 }
 
@@ -12,16 +12,34 @@ rdiscrete <- function(n, prob_mass, X) {
   #       - X: values of the random variable X in an ascending order.
   #output:- n random numbers that are distributed according to the given CDF
   #dependencies: rdiscr_position function
-  
+
   random <- runif(n)
   cum_dens <- cumsum(prob_mass)
   positions <- sapply(random, rdiscr_position, cum_dens = cum_dens)
   random_value <- X[positions]
-  
+
   return(random_value)
 }
 
 
+library(LaplacesDemon)
+
+rgenPois <- function(n, lambda, eta){
+  random_numbers <- numeric(n)
+
+  for (j in 1:n) {
+    index <- 0
+    unif <- runif(1)
+    nain <- dgpois(0, lambda = lambda, omega = eta)
+    while(nain < unif) {
+      index <- index + 1
+      nain <- nain + dgpois(index, lambda = lambda, omega = eta)
+    }
+    random_numbers[j] <- index
+  }
+  output <- list('rnumb' = random_numbers, 'cumDensity')
+  return(output)
+}
 
 
- 
+
