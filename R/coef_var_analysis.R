a7c7d6243db8e03e259357c297d337280e840a87
manuhuth <64041311+manuhuth@users.noreply.github.com>
2020-08-19 00:51:39 +0200

    Add files via upload

--- R/coef_var_analysis.R
+++ R/coef_var_analysis.R
@@ -1,16 +1,26 @@
-coef_var_analysis <- function(population, true_phi, M, sample_size, transform, iterations) {
+coef_var_analysis <- function(population, true_phi, M, sample_size, transform, iterations, numb_iterations, print_it = FALSE) {
   #input: - population: matrix containing the y variable as first column and the regressors as the other columns
   #       - true_phi: maatrix of the true eigenvectors
   #       - M: number of principal components that should be used
   #       - sample_size: size of the samples used for the analysis
   #       - transform: either 'no', 'normalized' or 'standardized'
   #       - iterations: number of iterations per estimate
+  #       - numb_iterations: number of iterations for empirical distribution of estimated of variance
   #output: - variances_beta_prac/theo: variances of the simulated beta in the stochastic/non-stochastic case using the empirical distribution
   #        - variances_beta_prac/theo_formula: variances of the simulated beta in the stochastic/non-stochastic case using formula 3.21
   #        - variances_Y_prac: Variances of Y hat in the stochastic case
   #        - variances_Y_theo: Variances of Y hat in the non-stochastic case
   #        - diff_Y_hat_changed: sum of differences in Y hat between stochastic and non-stochastic case
   #        - diff_beta_hat_changed: sum of differences in beta hat between stochastic and non-stochastic case
+
+  variances_beta_prac <- c()
+  variances_beta_prac_formula <- c()
+  variances_beta_prac_formula2 <- c()
+
+  variances_beta_theo <- c()
+  variances_beta_theo_formula <- c()
+
+  for (placeholder_index in 1:numb_iterations) {
 
   store_beta_theo <- c()
   store_beta_prac <- c()
@@ -38,7 +48,7 @@ coef_var_analysis <- function(population, true_phi, M, sample_size, transform, i
     Y_hat_prac_changed <- Z_prac_changed %*% beta_prac_changed
 
     #fixed true phi empirical variance
-    sample_theo <- population[sample(nrow(population), l), ]
+    sample_theo <- population[sample(nrow(population), sample_size), ]
     if (transform == 'normalized') {
       X_Sample_theo_demeaned <- scale(sample_theo[,c(-1)], TRUE, FALSE)
       Y_sample_theo_demeaned <- scale(sample_theo[,1], TRUE, FALSE)
@@ -56,15 +66,15 @@ coef_var_analysis <- function(population, true_phi, M, sample_size, transform, i
     #practical case with estimated phi at each iteration formula 2.21
     eigenvalues_hat_prac <-  PCR_sample_prac$eigenval
     error_estimate_prac <- PCR_sample_prac$Y_transformed - Y_hat_prac
-    variance_error_estimate_prac <- sum(error_estimate_prac^2) / (l - ncol(X_sample_prac)) #adjust by degrees of freedom
+    variance_error_estimate_prac <- sum(error_estimate_prac^2) / (sample_size - ncol(X_sample_prac)) #adjust by degrees of freedom
     variance_beta_prac_formula <- 1/eigenvalues_hat_prac * variance_error_estimate_prac / (sample_size-1) #adjust in code because
-    # the function PCR returns the eigenvalues from X'X/(n-1)
+    # the function PCR returns the eigenvalues of X'X/(n-1)
     variance_beta_prac_formula2 <- variance_error_estimate_prac * diag(solve(t(PCR_sample_prac$Z) %*% PCR_sample_prac$Z)) #check if computation is alright
 
 
     #fixed true phi formula 2.16
     error_estimate_theo <- Y_sample_theo_demeaned - Y_hat_theo
-    variance_error_estimate_theo <- sum(error_estimate_theo^2) / ( (l - ncol(X_sample_prac)))
+    variance_error_estimate_theo <- sum(error_estimate_theo^2) / ( (sample_size - ncol(X_sample_prac)))
     variance_beta_theo_formula <- variance_error_estimate_theo * diag(solve(t(Z_sample_theo) %*% Z_sample_theo))
 
     #store results
@@ -81,19 +91,143 @@ coef_var_analysis <- function(population, true_phi, M, sample_size, transform, i
     store_variance_beta_theo_formula <- rbind(store_variance_beta_theo_formula, t(variance_beta_theo_formula))
   }
 
-  #evaluate results
-  variances_beta_prac <- colVars(store_beta_prac)
-  variances_beta_prac_formula <- colMeans(store_variance_beta_prac_formula)
-  variances_beta_prac_formula2 <- colMeans(store_variance_beta_prac_formula2)
+    #evaluate results
+    variances_beta_prac <- rbind(variances_beta_prac, colVars(store_beta_prac))
+    variances_beta_prac_formula <- rbind(variances_beta_prac_formula, colMeans(store_variance_beta_prac_formula))
+    variances_beta_prac_formula2 <- rbind( variances_beta_prac_formula2, colMeans(store_variance_beta_prac_formula2))
 
-  variances_beta_theo <- colVars(store_beta_theo)
-  variances_beta_theo_formula <- colMeans(store_variance_beta_theo_formula)
+    variances_beta_theo <- rbind(variances_beta_theo, colVars(store_beta_theo))
+    variances_beta_theo_formula <- rbind(variances_beta_theo_formula, colMeans(store_variance_beta_theo_formula))
 
+    if (isTRUE(print_it)) {
+      print(sample_size)
+      print(placeholder_index)
+    }
+  }
   variances_Y_prac <- colVars(store_Y_hat_prac)
   variances_Y_theo <- colVars(store_Y_hat_theo)
 
-  diff_Y_hat_changed <- colSums(store_Y_hat_prac_changed - store_Y_hat_prac) #means from every iteration
-  diff_beta_hat_changed <- colSums(store_beta_prac_changed - store_beta_prac)
+  diff_Y_hat_changed <- colSums(abs(store_Y_hat_prac_changed - store_Y_hat_prac)) #means from every iteration
+  diff_beta_hat_changed <- colSums(abs(store_beta_prac_changed - store_beta_prac))
+
+  list_return <- list('variances_beta_prac' = variances_beta_prac, 'variances_beta_prac_formula' = variances_beta_prac_formula,
+                      'variances_beta_theo' =  variances_beta_theo, 'variances_beta_theo_formula' =  variances_beta_theo_formula,
+                      'variances_Y_prac' = variances_Y_prac, 'variances_Y_theo' = variances_Y_theo, 'diff_Y_hat_changed' = diff_Y_hat_changed,
+                      'diff_beta_hat_changed ' = diff_beta_hat_changed )
+  return(list_return)
+}
+
+
+coef_var_analysis_cl <- function(population, true_phi, M, sample_size, transform, iterations, print_it = FALSE) {
+  #input: - population: matrix containing the y variable as first column and the regressors as the other columns
+  #       - true_phi: maatrix of the true eigenvectors
+  #       - M: number of principal components that should be used
+  #       - sample_size: size of the samples used for the analysis
+  #       - transform: either 'no', 'normalized' or 'standardized'
+  #       - iterations: number of iterations per estimate
+  #output: - variances_beta_prac/theo: variances of the simulated beta in the stochastic/non-stochastic case using the empirical distribution
+  #        - variances_beta_prac/theo_formula: variances of the simulated beta in the stochastic/non-stochastic case using formula 3.21
+  #        - variances_Y_prac: Variances of Y hat in the stochastic case
+  #        - variances_Y_theo: Variances of Y hat in the non-stochastic case
+  #        - diff_Y_hat_changed: sum of differences in Y hat between stochastic and non-stochastic case
+  #        - diff_beta_hat_changed: sum of differences in beta hat between stochastic and non-stochastic case
+
+  variances_beta_prac <- c()
+  variances_beta_prac_formula <- c()
+  variances_beta_prac_formula2 <- c()
+
+  variances_beta_theo <- c()
+  variances_beta_theo_formula <- c()
+
+  for (placeholder_index in 1:1) {
+
+    store_beta_theo <- c()
+    store_beta_prac <- c()
+    store_Y_hat_theo <- c()
+    store_Y_hat_prac <- c()
+    store_Y_hat_prac_changed <- c()
+    store_beta_prac_changed <- c()
+    store_variance_beta_prac_formula <- c()
+    store_variance_beta_prac_formula2 <- c()
+    store_variance_beta_theo_formula <- c()
+
+    for (k in 1:iterations) {
+      #practical case with estimated phi at each iteration empirical variance
+      sample_prac <- population[sample(nrow(population), sample_size), ]
+      Y_sample_prac <- sample_prac[ , 1]
+      X_sample_prac <- sample_prac[ , c(-1)]
+      PCR_sample_prac <- PCR(Y = Y_sample_prac, X = X_sample_prac, M = M, transform_Y = trans, transform_X = trans)
+      beta_prac <- PCR_sample_prac$beta_Z
+      Y_hat_prac <- PCR_sample_prac$Y_hat
+
+      #check if Y_hat changes with other representant of [\hat{phi}]
+      phi_prac_changed <- PCR_sample_prac$phi %*% diag(c(-1, rep( 1, ncol(PCR_sample_prac$phi)-1) ) ) #first column times minus one
+      Z_prac_changed <- PCR_sample_prac$X_transformed %*% phi_prac_changed
+      beta_prac_changed <- solve(t(Z_prac_changed) %*% Z_prac_changed ) %*% t(Z_prac_changed) %*% Y_hat_prac
+      Y_hat_prac_changed <- Z_prac_changed %*% beta_prac_changed
+
+      #fixed true phi empirical variance
+      sample_theo <- population[sample(nrow(population), sample_size), ]
+      if (transform == 'normalized') {
+        X_Sample_theo_demeaned <- scale(sample_theo[,c(-1)], TRUE, FALSE)
+        Y_sample_theo_demeaned <- scale(sample_theo[,1], TRUE, FALSE)
+      }
+
+      if (transform == 'standardized') {
+        X_Sample_theo_demeaned <- scale(sample_theo[,c(-1)], TRUE, TRUE)
+        Y_sample_theo_demeaned <- scale(sample_theo[,1], TRUE, TRUE)
+      }
+
+      Z_sample_theo <- X_Sample_theo_demeaned %*% true_phi
+      beta_theo <- solve(t(Z_sample_theo) %*% Z_sample_theo) %*% t(Z_sample_theo) %*% Y_sample_theo_demeaned
+      Y_hat_theo <- Z_sample_theo %*% beta_theo
+
+      #practical case with estimated phi at each iteration formula 2.21
+      eigenvalues_hat_prac <-  PCR_sample_prac$eigenval
+      error_estimate_prac <- PCR_sample_prac$Y_transformed - Y_hat_prac
+      variance_error_estimate_prac <- sum(error_estimate_prac^2) / (sample_size - ncol(X_sample_prac)) #adjust by degrees of freedom
+      variance_beta_prac_formula <- 1/eigenvalues_hat_prac * variance_error_estimate_prac / (sample_size-1) #adjust in code because
+      # the function PCR returns the eigenvalues from X'X/(n-1)
+      variance_beta_prac_formula2 <- variance_error_estimate_prac * diag(solve(t(PCR_sample_prac$Z) %*% PCR_sample_prac$Z)) #check if computation is alright
+
+
+      #fixed true phi formula 2.16
+      error_estimate_theo <- Y_sample_theo_demeaned - Y_hat_theo
+      variance_error_estimate_theo <- sum(error_estimate_theo^2) / ( (sample_size - ncol(X_sample_prac)))
+      variance_beta_theo_formula <- variance_error_estimate_theo * diag(solve(t(Z_sample_theo) %*% Z_sample_theo))
+
+      #store results
+      store_Y_hat_prac <- cbind(store_Y_hat_prac, Y_hat_prac) # ys in columns for each sample -> colVars works
+      store_Y_hat_theo <- cbind(store_Y_hat_theo, Y_hat_theo)
+      store_Y_hat_prac_changed <- cbind(store_Y_hat_prac_changed, Y_hat_prac_changed)
+
+      store_beta_prac <- rbind(store_beta_prac, t(beta_prac)) # betas in columns -> colVars works
+      store_beta_theo <- rbind(store_beta_theo, t(beta_theo))
+      store_beta_prac_changed <- rbind(store_beta_prac_changed, t(beta_prac_changed))
+
+      store_variance_beta_prac_formula <- rbind(store_variance_beta_prac_formula, t(variance_beta_prac_formula))
+      store_variance_beta_prac_formula2 <- rbind(store_variance_beta_prac_formula2, t(variance_beta_prac_formula2))
+      store_variance_beta_theo_formula <- rbind(store_variance_beta_theo_formula, t(variance_beta_theo_formula))
+    }
+
+    #evaluate results
+    variances_beta_prac <- rbind(variances_beta_prac, store_beta_prac)
+    variances_beta_prac_formula <- rbind(variances_beta_prac_formula, store_variance_beta_prac_formula)
+    variances_beta_prac_formula2 <- rbind( variances_beta_prac_formula2, store_variance_beta_prac_formula2)
+
+    variances_beta_theo <- rbind(variances_beta_theo, store_beta_theo)
+    variances_beta_theo_formula <- rbind(variances_beta_theo_formula, store_variance_beta_theo_formula)
+
+    if (isTRUE(print_it)) {
+      print(sample_size)
+      print(placeholder_index)
+    }
+  }
+  variances_Y_prac <- colVars(store_Y_hat_prac)
+  variances_Y_theo <- colVars(store_Y_hat_theo)
+
+  diff_Y_hat_changed <- colSums(abs(store_Y_hat_prac_changed - store_Y_hat_prac)) #means from every iteration
+  diff_beta_hat_changed <- colSums(abs(store_beta_prac_changed - store_beta_prac))
 
   list_return <- list('variances_beta_prac' = variances_beta_prac, 'variances_beta_prac_formula' = variances_beta_prac_formula,
                       'variances_beta_theo' =  variances_beta_theo, 'variances_beta_theo_formula' =  variances_beta_theo_formula,
--- R/plot_functions.R
+++ R/plot_functions.R
@@ -5,7 +5,7 @@ plot_2D_data <- function(sample_size, true_phi, true_X, legend, transform) {
   #       - legend: TRUE or FALSE indicates whether each plot should have a legend
   #output: - plots showing the deviations of the eigenvectors from the true eigenvectors
 
-  X_sample_2D <- X_2D[sample(nrow(true_X), sample_size), ] #draw sample
+  X_sample_2D <- true_X[sample(nrow(true_X), sample_size), ] #draw sample
   pc_sample_2D <- PCA(X_sample_2D, transform = transform) #compute pc of sample
   phi_sample_2D <- pc_sample_2D$phi #get eigenvectors of sample
   if (isTRUE(legend)) {
@@ -25,4 +25,188 @@ plot_2D_data <- function(sample_size, true_phi, true_X, legend, transform) {
       geom_abline(intercept = 0, slope = phi_sample_2D[1,2]/phi_sample_2D[2,2], colour = 'red') +
       ggtitle(paste('N = ', sample_size, sep = ""))} +
     theme(plot.title = element_text(hjust = 0.5))
+}
+
+plots_variance <- function(df_1, legend_df1, colour_1 = 'orange', df_2, legend_df2, colour_2 = 'blue',
+                           sample_size, xlab_string, ylab_string, numb_columns, numb_plots = NULL, height = c(1, 0.2)) {
+  #input: - df_1/2: data frame that consists of the variances to be plotted in one column. One sample size per column
+  #       - legend_df1/2: name displayed in the legend
+  #       - colour_1/2: colour of line plot
+  #       - sample_size: vector of sample sizes in the columns of df_1/2
+  #       - x/ylab_string: name of the xlab/ylab
+  #       - numb_columns: Number of plots in each row in the final plot
+  #       - numb_plots: Number of columns for which the columns should be plotted
+  #output: plots of each columns of df_1 and df_2 in one graph
+
+  plot_list <- list()
+  if (is.null(numb_plots)) {
+    numb_plots <- ncol(df_1)
+  }
+
+  for (index in 1:numb_plots) {
+    df <- as.data.frame(cbind(df_1[, index], df_2[,index], sample_size))
+    colnames(df) <- c('first', 'second', 'sample_size')
+    plot_list[[index]] <- ggplot(df, aes(x=sample_size, y=first, colour = legend_df1)) + geom_line() +
+      geom_line(aes(x=sample_size, y=second, colour = legend_df2)) + ggtitle(paste('beta', index, sep = "")) +
+      xlab(xlab_string) + ylab(ylab_string) + labs(colour = '') +
+      scale_colour_manual(values=c(colour_2,colour_1)) +
+      theme(plot.title = element_text(hjust = 0.5), legend.position = 'none')
+  }
+
+  legend <- cowplot::get_legend(plot_list[[1]] + theme(legend.position = "bottom")) #cowplot::otherwise cowplot's ggplot theme is loaded
+  p_grid <- cowplot::plot_grid(plotlist = plot_list, ncol = numb_columns)
+  cowplot::plot_grid(p_grid, legend, ncol = 1, rel_heights = height)
+}
+
+
+prepare_variance_plots <- function(list_coef_var_analysis, sample_size, confidence) {
+  #input: - list_coef_var_analysis: object created by prepare_variance_plots function
+  #       - sample_size: vector containing the sample sizes
+  #       - confidence: confidence level in which the variance estimate should be
+  #output: - means_XXX: means of the simulated variance estimates
+  #        - lower/upper_XXX: lower bounds and upper bounds in which confidence percent of the variacne estimates are
+  means_cl_prac_formula <- c()
+  upper_cl_prac_formula <- c()
+  lower_cl_prac_formula <- c()
+  means_cl_theo_formula <- c()
+  upper_cl_theo_formula <- c()
+  lower_cl_theo_formula <- c()
+  means_cl_prac <- c()
+  means_cl_theo <- c()
+
+  for (index in 1:length(list_coef_var_analysis)) {
+    help_mean_prac_formula <- colMeans(list_coef_var_analysis[[index]]$variances_beta_prac_formula)
+    means_cl_prac_formula <- rbind(means_cl_prac_formula,  t(help_mean_prac_formula))
+    help_var_prac_formula <- colVars(list_coef_var_analysis[[index]]$variances_beta_prac_formula) / sample_size[index] #variance of estimator
+    upper_cl_prac_formula <- rbind( upper_cl_prac_formula, t(help_mean_prac_formula + qnorm( confidence + (1-confidence)/2 ) * help_var_prac_formula^0.5) ) #upper bound CI
+    lower_cl_prac_formula <- rbind( lower_cl_prac_formula, t(help_mean_prac_formula - qnorm( confidence + (1-confidence)/2 ) * help_var_prac_formula^0.5) ) #lower bound CI
+
+    help_mean_theo_formula <- colMeans(list_coef_var_analysis[[index]]$variances_beta_theo_formula)
+    means_cl_theo_formula <- rbind(means_cl_theo_formula, t(help_mean_theo_formula) )
+    help_var_theo_formula <- colVars(list_coef_var_analysis[[index]]$variances_beta_theo_formula) / sample_size[index] #variance of estimator
+    upper_cl_theo_formula <- rbind( upper_cl_theo_formula, t(help_mean_theo_formula + qnorm( confidence + (1-confidence)/2 ) * help_var_theo_formula^0.5) ) #upper bound CI
+    lower_cl_theo_formula <- rbind( lower_cl_theo_formula, t(help_mean_theo_formula - qnorm( confidence + (1-confidence)/2 ) * help_var_theo_formula^0.5) ) #lower bound CI
+
+    means_cl_prac <- rbind(means_cl_prac, t(colVars(list_coef_var_analysis[[index]]$variances_beta_prac) ) )
+    means_cl_theo <- rbind(means_cl_theo, t(colVars(list_coef_var_analysis[[index]]$variances_beta_theo) ) )
+  }
+    list_return <- list('means_cl_prac_formula' = means_cl_prac_formula, 'upper_cl_prac_formula ' = upper_cl_prac_formula, 'lower_cl_prac_formula' = lower_cl_prac_formula,
+                        'means_cl_theo_formula' =  means_cl_theo_formula, 'upper_cl_theo_formula' = upper_cl_theo_formula, 'lower_cl_theo_formula' = lower_cl_theo_formula,
+                        'means_cl_prac' = means_cl_prac, 'means_cl_theo' = means_cl_theo)
+    return(list_return)
+
+  #end of shape function
+}
+
+#Plots with CI
+prepare_variances_plots_CI <- function(list_variances, sample_size, beta_column, confidence, type_CI = 'mean', numb_it) {
+  #input: - list_variances: object created by prepare_variance_plots function
+  #       - sample_size: vector containing the sample sizes
+  #       - beta_column: index for which beta the plot should be prepared
+  #       - confidence: confidence level in which the variance estimate should be
+  #output: - 4 data frames containing the lower/upper bounds and the means of the variances
+    low <- (1-confidence)/2
+    up <- (1-confidence)/2 + confidence
+    store_beta_prac <- c()
+    store_beta_prac_formula <- c()
+    store_beta_theo <- c()
+    store_beta_theo_formula <- c()
+
+    for (index in 1:25) {
+      prac_mean <- mean(list_variances[[index]]$variances_beta_prac[,beta_column])
+      if (type_CI == 'mean') {
+        prac_sd <- var(list_variances[[index]]$variances_beta_prac[,beta_column])^0.5 /numb_it
+        prac_up <- prac_mean + qnorm(up, prac_mean, prac_sd)*prac_sd
+        prac_low <- prac_mean - qnorm(up, prac_mean, prac_sd)*prac_sd
+      } else{
+        prac_up <- quantile(list_variances[[index]]$variances_beta_prac[,beta_column], up)
+        prac_low <- quantile(list_variances[[index]]$variances_beta_prac[,beta_column], low)
+      }
+
+
+      prac_formula_mean <- mean(list_variances[[index]]$variances_beta_prac_formula[,beta_column])
+      if (type_CI == 'mean') {
+        prac_formula_sd <- var(list_variances[[index]]$variances_beta_prac_formula[,beta_column])^0.5 /numb_it
+        prac_formula_up <- prac_formula_mean + qnorm(up, prac_formula_mean, prac_formula_sd)*prac_formula_sd
+        prac_formula_low <- prac_formula_mean - qnorm(up, prac_formula_mean, prac_formula_sd)*prac_formula_sd
+      } else{
+      prac_formula_up <-quantile(list_variances[[index]]$variances_beta_prac_formula[,beta_column], up)
+      prac_formula_low <- quantile(list_variances[[index]]$variances_beta_prac_formula[,beta_column], low)
+      }
+
+
+      theo_mean <- mean(list_variances[[index]]$variances_beta_theo[,beta_column])
+      if (type_CI == 'mean') {
+        theo_sd <- var(list_variances[[index]]$variances_beta_theo[,beta_column])^0.5 /numb_it
+        theo_up <- theo_mean + qnorm(up, theo_mean, theo_sd)*theo_sd
+        theo_low <- theo_mean - qnorm(up, theo_mean, theo_sd)*theo_sd
+      } else{
+        theo_up <- quantile(list_variances[[index]]$variances_beta_theo[,beta_column], up)
+        theo_low <- quantile(list_variances[[index]]$variances_beta_theo[,beta_column], low)
+      }
+
+      theo_formula_mean <- mean(list_variances[[index]]$variances_beta_theo_formula[,beta_column])
+      if (type_CI == 'mean') {
+        theo_formula_sd <- var(list_variances[[index]]$variances_beta_theo_formula[,beta_column])^0.5 /numb_it
+        theo_formula_up <- theo_formula_mean + qnorm(up, theo_formula_mean, theo_formula_sd)*theo_formula_sd
+        theo_formula_low <- theo_formula_mean - qnorm(up, theo_formula_mean, theo_formula_sd)*theo_formula_sd
+      } else{
+      theo_formula_up <-quantile(list_variances[[index]]$variances_beta_theo_formula[,beta_column], up)
+      theo_formula_low <- quantile(list_variances[[index]]$variances_beta_theo_formula[,beta_column], low)
+      }
+
+      store_beta_prac <- rbind(store_beta_prac, cbind(prac_up,prac_mean, prac_low))
+      store_beta_prac_formula <- rbind(store_beta_prac_formula, cbind(prac_formula_up,prac_formula_mean, prac_formula_low))
+      store_beta_theo <- rbind(store_beta_theo, cbind(theo_up,theo_mean, theo_low))
+      store_beta_theo_formula <- rbind(store_beta_theo_formula, cbind(theo_formula_up,theo_formula_mean, theo_formula_low))
+
+    }
+    colnames(store_beta_prac_formula) <- c('up','mean', 'low')
+    colnames(store_beta_prac) <- c('up','mean', 'low')
+    colnames(store_beta_theo_formula) <- c('up','mean', 'low')
+    colnames(store_beta_theo) <- c('up','mean', 'low')
+    list_return <- list('beta_prac_formula' = store_beta_prac_formula, 'beta_prac' = store_beta_prac,
+                        'beta_theo_formula' = store_beta_theo_formula, 'beta_theo' = store_beta_theo)
+    return(list_return)
+}
+
+plots_variance_CI <- function(variance_plots_data, first_method, second_method, colour_1, colour_2,
+                              xlab_string, ylab_string, legend_df1, legend_df2, height = c(1,0.2),
+                              numb_columns, numb_plots = NULL, sample_size, position_ylab = 0.6) {
+  #input: - variance_plots_data: list created by prepare_variances_plots_CI
+  #       - first/second_method: either c(beta_prac, beta_prac_formula, beta_theo, beta_theo_formula)
+  #       - colour_1/2: colour of line plot
+  #       - sample_size: vector of sample sizes in the columns of df_1/2
+  #       - x/ylab_string: name of the xlab/ylab
+  #       - legend_df1/2: name displayed in the legend
+  #       - numb_columns: Number of plots in each row in the final plot
+  #       - numb_plots: Number of columns for which the columns should be plotted
+  #       - height: scaling of the plot window
+  #       - sample_size: vector containing the sample size
+  #output: plots of each columns of the data in variance_plots_data
+  plot_list <- list()
+
+  for (index in 1:length(variance_plots_data)) {
+    df1 <- as.data.frame(variance_plots_data[[index]][first_method])
+    colnames(df1) <- c('up', 'mean', 'low')
+    df2 <- as.data.frame(variance_plots_data[[index]][second_method])
+    colnames(df2) <- c('up', 'mean', 'low')
+    df <- as.data.frame(cbind(df1[,'up'],df1[,'mean'], df1[,'low'], df2[,'up'],df2[,'mean'], df2[,'low'], sample_size))
+
+    colnames(df) <- c('up1', 'mean1', 'low1', 'up2', 'mean2', 'low2', 'sample_size')
+    plot_list[[index]] <- ggplot(data = df, aes(sample_size, up1)) +
+      geom_ribbon(data=df,aes(ymin=low1,ymax=up1),alpha=0.3, fill = colour_1) +
+      geom_ribbon(data=df,aes(ymin=low2,ymax=up2),alpha=0.3, fill = colour_2) +
+      geom_line(data = df, aes(sample_size, mean1,colour = legend_df1)) +
+      geom_line(data = df, aes(sample_size, mean2,colour = legend_df2)) +
+      ggtitle(paste('beta', index, sep = "")) +
+      xlab(xlab_string) + ylab(' ') + labs(colour = '') +
+      scale_colour_manual(values=c(colour_2, colour_1)) +
+      theme(plot.title = element_text(hjust = 0.5), legend.position = 'none')
+  }
+  legend <- cowplot::get_legend(plot_list[[1]] + theme(legend.position = "bottom")) #cowplot::otherwise cowplot's ggplot theme is loaded
+  p_grid <- cowplot::plot_grid(plotlist = plot_list, ncol = numb_columns)
+  cowplot::plot_grid(p_grid, legend, ncol = 1, rel_heights = height) +
+    cowplot::draw_label(ylab_string, x=  0, y=position_ylab, vjust= 1, angle=90)
+
 }
